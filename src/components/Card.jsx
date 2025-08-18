8a55827aef3b117c12399933dc542aa0ed04d6d5
NandkishorJadoun <183695114+NandkishorJadoun@users.noreply.github.com>
2025-08-18 19:48:39 +0530

    feat: add className to card

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -1,6 +1,6 @@
 export default function Card({ name, image, handleCardClick }) {
   return (
-    <div onClick={handleCardClick}>
+    <div className="card" onClick={handleCardClick}>
       <img src={image} alt={name} />
       <p>{name}</p>
     </div>
